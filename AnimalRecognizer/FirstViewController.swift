cbbb8efd3c3b5f534077fe5948e52e200e0969cf
Ignacio Acisclo Pérez <iacisclo@autentia.com>
2020-05-22 09:20:04 +0200

    Initial Commit

---
+++ AnimalRecognizer/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  AnimalRecognizer
+//
+//  Created by Ignacio Acisclo on 22/05/2020.
+//  Copyright © 2020 iAcisclo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AnimalRecognizer/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  AnimalRecognizer
+//
+//  Created by Ignacio Acisclo on 22/05/2020.
+//  Copyright © 2020 iAcisclo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
